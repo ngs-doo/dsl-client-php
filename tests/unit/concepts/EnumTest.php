bd3e222294f08f01ec59e8516ea72327d6ad84b5
nutrija <viktor.jagusic@gmail.com>
2014-01-10 17:25:25 +0100

    Enum concept
    Enum values are strings implemented as constants in abstract class

---
+++ tests/unit/concepts/EnumTest.php
@@ -0,0 +1,64 @@
+<?php
+use Img\Extension;
+use Img\Bitmap;
+use Img\Icon;
+use Img\Album;
+
+class EnumTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetEnumPropertyOnEntity()
+    {
+        $img = new Bitmap();
+        $img->ext = Img\Extension::jpg;
+        
+        $this->assertSame(Img\Extension::jpg, $img->ext);
+        
+        $img->persist();
+        $persisted = Bitmap::find($img->URI);
+        
+        $this->assertSame(Img\Extension::jpg, $persisted->ext);
+    }
+    
+    public function testSetEnumPropertyOnValue()
+    {
+        $val = new Icon();
+        $val->ext = Img\Extension::png;
+        
+        $this->assertSame(Img\Extension::png, $val->ext);
+    }
+    
+    public function testEnumPropertyDefaultValue()
+    {
+        $img = new Bitmap();
+        $this->assertSame(Img\Extension::jpg, $img->ext);
+    }
+    
+    public function testEnumGetAllValues()
+    {
+        $expected = array('jpg', 'png', 'bmp');
+        $this->assertSame($expected, Extension::getValues());
+    }
+    
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetInvalid()
+    {
+        $img = new Icon();
+        
+        $img->ext = 'invalid-enum';
+    }
+    
+    public function testCollectionProperty()
+    {
+        $album = new Album();
+        $assigned = array('jpg', 'bmp');
+        $album->allowed = $assigned;
+        
+        $album->persist();
+        
+        $found = Album::find($album->URI);
+        
+        $this->assertSame($assigned, $found->allowed);
+    }
+}
